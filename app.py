e1b246b0abb117c8770af76a08ab075858352c77
nello0203-lgtm <nello0203@gmail.com>
2026-04-09 20:13:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,4 +121,8 @@ def analyze_batch():
             "uncertainty": res['uncertainty']
         })
         
-    return jsonify({"status": "success", "
+    return jsonify({"status": "success", "results": results})
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
